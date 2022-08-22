554b4dc1697249eb6b47416f928a05d2278227ca
23dgupta <durgeshkumar91g@gmail.com>
2022-08-22 20:36:27 +0530

    PoP up design form completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,62 @@
 import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import Swal from 'sweetalert2';
+import swalWithBootstrapButtons from 'sweetalert2';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  title = 'popform';
-  content:string=''
+export class AppComponent implements OnInit {
+  title = 'User Registration';
+  content: string = ''
 
-  constructor(private toast:ToastrService,private modalService: NgbModal){
+  addForm: FormGroup |any
+
+  constructor(private toast: ToastrService, private modalService: NgbModal) {
 
   }
-  
+
   ngOnInit() {
-    // this.toast.success("Done","User");
-    // this.toast.warning("warning","User");
-    // this.toast.error("error","User");
-    // this.toast.info("info","User");
+    this.setFormState();
   }
+setFormState(){
 
-  openXl(content:any) {
+  this.addForm = new FormGroup({
+    id: new FormControl(0),
+    title: new FormControl('', Validators.required),
+    firstName: new FormControl('', Validators.compose([Validators.required, Validators.minLength(3),Validators.maxLength(10)])),
+    lastName: new FormControl('', Validators.compose([Validators.required, Validators.minLength(3),Validators.maxLength(10)])),
+    email: new FormControl('', Validators.compose([Validators.required, Validators.email])),
+    //dob:new FormControl('', Validators.compose([Validators.required, Validators.pattern('/^((1[0-2])|[1-9])[/-.](3[0-1]|[1-2]\d|[1-9])[/-.][19|20]\d{2}$/')])),
+    dob:new FormControl('', Validators.compose([Validators.required])),
+    password:new FormControl('', Validators.compose([Validators.required, Validators.minLength(8),Validators.maxLength(16)])),
+    confirmPassword:new FormControl('', Validators.compose([Validators.required])),
+    acceptTerms:new FormControl(false,Validators.required)
+  });
+
+
+}
+
+  edit(id: number) {
+    alert("edit call : " + id)
+  }
+  delete(id: number) {
+    alert("delete call : " + id)
+
+  }
+  openXl(content: any) {
     this.modalService.open(content, { size: 'xl' });
   }
 
- 
+  addUser(){
+
+    console.log(this.addForm.value);
+    console.log(this.addForm.valid);
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import {  NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import {  NgbModule } from '@ng-bootstrap/ng-bootstrap';
     BrowserAnimationsModule,
     ToastrModule.forRoot(),
     NgbModule,
+    ReactiveFormsModule
   
   ],
   providers: [],
