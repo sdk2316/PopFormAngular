7ec071c102b9ae6f4b4a19c9d6c393a9d6a91b7e
23dgupta <durgeshkumar91g@gmail.com>
2022-08-22 22:17:26 +0530

    custom form validation done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { ToastrService } from 'ngx-toastr';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import Swal from 'sweetalert2';
 import swalWithBootstrapButtons from 'sweetalert2';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl,FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +13,8 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent implements OnInit {
   title = 'User Registration';
   content: string = ''
+
+  submitted:boolean=false;
 
   addForm: FormGroup |any
 
@@ -34,11 +36,26 @@ setFormState(){
     //dob:new FormControl('', Validators.compose([Validators.required, Validators.pattern('/^((1[0-2])|[1-9])[/-.](3[0-1]|[1-2]\d|[1-9])[/-.][19|20]\d{2}$/')])),
     dob:new FormControl('', Validators.compose([Validators.required])),
     password:new FormControl('', Validators.compose([Validators.required, Validators.minLength(8),Validators.maxLength(16)])),
-    confirmPassword:new FormControl('', Validators.compose([Validators.required])),
+    confirmPassword:new FormControl('', Validators.compose([Validators.requiredTrue])),
     acceptTerms:new FormControl(false,Validators.required)
   });
 
 
+}
+
+
+cancelForm(){
+  this.addForm.reset({
+    id: 0,
+    title: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+    dob:'',
+    password:'',
+    confirmPassword:'',
+    acceptTerms:false
+  });
 }
 
   edit(id: number) {
@@ -53,6 +70,7 @@ setFormState(){
   }
 
   addUser(){
+    this.submitted=true
 
     console.log(this.addForm.value);
     console.log(this.addForm.valid);
